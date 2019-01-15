c69e6738591201746533a0b8802e2f432332c7cc
“Susmit07” <susmitsircar@gmail.com>
2019-01-15 11:27:07 +0530

    first commit forswagger documentation for rest services in spring boot

---
+++ src/main/java/com/sapient/swagger/swaggerexamples/SwaggerExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.swagger.swaggerexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerExamplesApplication.class, args);
+	}
+}
+
---
+++ src/test/java/com/sapient/swagger/swaggerexamples/SwaggerExamplesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sapient.swagger.swaggerexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
